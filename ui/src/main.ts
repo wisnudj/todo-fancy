7e1a4541de35e4e2ecd307c174969204ecfb5e3a
wisnudj <jayalahwisnu@gmail.com>
2026-02-08 20:28:00 +0700

    add ui for new development

---
+++ ui/src/main.ts
@@ -0,0 +1,12 @@
+import 'bulma/css/bulma.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ ui/src/views/LoginView.vue
@@ -0,0 +1,51 @@
+<template>
+<section class="hero is-bold">
+    <div class="hero-body">
+      <div class="container pt-6">
+        <div class="columns is-centered">
+          <div class="column is-half-tablet is-4-desktop">
+            <div class="card">
+              <div class="card-content">
+                <div class="content">
+                  <h2 class="has-text-centered has-text-weight-bold mb-2">Todo App</h2>
+                  <p class="has-text-centered is-size-6 has-text-grey mb-5">Kelola tugas harianmu dengan sederhana</p>
+                  <div class="tabs is-toggle is-fullwidth">
+                    <ul class="ml-0">
+                      <li class="is-active"><a>Login</a></li>
+                      <li><a>Register</a></li>
+                    </ul>
+                  </div>
+                  <form action="">
+                    <div class="field">
+                      <label class="label">Email</label>
+                      <div class="control has-icons-left">
+                        <input type="email" class="input" placeholder="email@example.com" />
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-envelope"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Password</label>
+                      <div class="control has-icons-left">
+                        <input type="password" class="input" placeholder="********" />
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-lock"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="field mt-3">
+                      <div class="control">
+                        <button class="button is-primary is-fullwidth">Login</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
